31785425dfdc700d97ac8de79702550be85e81bf
aniko33 <76649588+aniko33@users.noreply.github.com>
2023-05-03 00:08:09 +0200

    Delete site directory

--- site/js/alert.js
+++
@@ -1,3 +0,0 @@
-console.log("%cHold Up!", "color: cyan; font-size: 40px")
-console.log("%cBe careful of what you copy and paste into the console. They can STEAL your account.",
-"color: red; font-size: 20px");
--- site/js/loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    loader.classList.add("loader-hide");
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(loader);
-    })
-})
